b3ec595e682d3ec6f95f720f101178031716fe51
Jim-Lin <jim_lin@migocorp.com>
2015-01-05 14:02:19 +0800

    modify customer

---
+++ FirstWebApi/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstWebApi.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string CustName { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; }
+
+        [Required]
+        public DateTime Modified { get; set; }
+
+        public IList<Order> Orders { get; set; }
+    }
+}
---
+++ FirstWebApi/Modules/CustomerMetadataModule.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstWebApi.Modules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using FirstWebApi.Models;
+    using Nancy.Metadata.Module;
+    using Nancy.Swagger;
+
+    public class CustomerMetadataModule : MetadataModule<SwaggerRouteData>
+    {
+        public CustomerMetadataModule()
+        {
+            this.Describe["GetCustomers"] = description => description.AsSwagger(with =>
+            {
+                with.ResourcePath("/customer");
+                with.Summary("The list of customers");
+                with.Notes("This returns a list of customers");
+                with.Model<Customer>();
+            });
+        }
+    }
+}
