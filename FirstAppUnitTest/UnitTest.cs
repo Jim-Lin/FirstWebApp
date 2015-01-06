090ee1b6fe4b9b345ef2f3dce4f78e8a9afe2d18
Jim-Lin <jim_lin@migocorp.com>
2015-01-06 14:14:14 +0800

    add FirstAppUnitTest proj

---
+++ FirstAppUnitTest/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstAppUnitTest
+{
+    using System;
+    using FirstWebApp;
+    using FirstWebApp.Services;
+    using Xunit;
+
+    public class UnitTest
+    {
+        private static FirstEntities entities = new FirstEntities();
+        private AppService<Customer> custService = new CustomerService(entities);
+
+        [Fact]
+        public void TestCustName()
+        {
+            Assert.Equal("JimLin", this.custService.GetRecordById(2).CustName);
+        }
+    }
+}
